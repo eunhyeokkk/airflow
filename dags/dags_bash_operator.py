c89932ccb7a35d902d9bbf8b37107528e1fb5f9d
eunhyeok8903 <eunhyeok8903@gmail.com>
2025-01-26 02:15:55 +0900

    TEMP

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -1,4 +1,5 @@
-from airflow import DAG
+
+from airflow.models.dag import DAG
 import datetime
 import pendulum
 from airflow.operators.bash import BashOperator
@@ -6,10 +7,9 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="example_bash_operator", #airflow UI에서 보여주는 DAG이름, 통상적으론 py파일명과 동일하게설정
     schedule="0 0 * * *", #분 시 일 월 요일
-    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
-    #task
     bash_t1 = BashOperator(
         task_id="bash_t1",
         bash_command="echo TEST",
